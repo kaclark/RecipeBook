f4ce5d62f48820e2d61a96d5fcb4cead8aa8b209
Keanu Clark <da1andonlyone13@gmail.com>
2022-04-09 22:23:28 -0400

    comments added

---
+++ README.md
@@ -0,0 +1,25 @@
+# RecipeBook
+Recipes are saved in files
+Recipes are loaded from that
+
+Recipe file format
+&N
+Name of Dish
+&E
+Equipment #1
+...
+Equipment #N
+&I
+Ingredient #1
+...
+Ingredient #M
+&S
+Step #1
+...
+Step #K
+
+Desired Features/TODO:
+Pick a random recipie
+Make and save collections of recipies
+Terminal action, so that file does not need to opened
+Help terminal blurb
--- recipebook_main.py
+++ recipebook_main.py
@@ -1,11 +1,15 @@
+#recipie class that will be handling all the information for a single recipie 
 class recipe:
     
+    #There is a default, empty form that this can be initialized into for loading recipe from file
+    #This is how this class is expected to be used for the most part
     def __init__(self, name=None, equipment=[], ingredients=[], steps=[]):
         self.name = name
         self.equipment = equipment
         self.ingredients = ingredients
         self.steps = steps
-
+    
+    #The summary prints everything that makes up the recipie 
     def summary(self):
         print(self.name.upper())
         print("Equipment")
@@ -18,8 +22,11 @@ class recipe:
         for ind, step in enumerate(self.steps):
             print(" ", str(ind + 1) + ")", step)
 
+    
     def load_from_file(self, rec_name):
+        #recipies should all be stored in rec dir
         with open("rec/" + rec_name + ".rec", "r") as rec_in:
+            #list comp: remove newline character from each line
             raw_rec = [l.split('\n')[0] for l in rec_in.readlines()]
         #mark the zones for recording name, equipment, ingredients, and steps
         n_l = -1; e_l = -1; i_l = -1; s_l = -1
@@ -32,16 +39,22 @@ class recipe:
                 i_l = l_n
             if "&S" in line:
                 s_l = l_n
+        #name is on row after name tag
         self.name = raw_rec[n_l + 1]
+        #rows after equipment tag and up to ingredients tag are equipment entries
         for x_e in range(e_l + 1, i_l):
             self.equipment.append(raw_rec[x_e])
+        #rows after ingredients tag up to the steps tag are ingredient entries
         for x_i in range(i_l + 1, s_l):
             self.ingredients.append(raw_rec[x_i])
+        #all rows after steps tag are step entries
         for x_s in range(s_l + 1, len(raw_rec)):
             self.steps.append(raw_rec[x_s])
+        #After loading the file in, the present desired behavior is to just give a summary
         self.summary()
 
 """
+Example of how to load in a recipe in code
 pan_fry_chicken = recipe(
     "pan fried chicken", 
     ["pan", "stove", "stirring utensil"],
@@ -59,6 +72,8 @@ pan_fry_chicken = recipe(
     )
 
 pan_fry_chicken.summary()
-"""
+
+Example of how to load in a recipe from file
 pan_fried_chicken = recipe()
 pan_fried_chicken.load_from_file("pan_fried_chicken")
+"""
