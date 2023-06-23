383d243377eecede7159008422ae3a5c717f4bc4
Keanu Clark <da1andonlyone13@gmail.com>
2023-06-23 19:02:57 -0400

    index generation now generalized and poised for rec pages to populate routes dir

--- gen_html_from_rec.py
+++ gen_html_from_rec.py
@@ -11,29 +11,46 @@ def get_recs(file_extension=False):
             recs.append(str(x_path).split("/")[1].split(".rec")[0])
     return recs
 
-def construct_index():
-    html_top = ""
-    with open("./include/boiler_html_elements/html_top.txt", "r") as h_top:
-        lines = h_top.readlines()
-        for line in lines:
-            html_top += line
+def construct_index(title, subtitle, img_src):
 
     def list_recipe(rec):
         return "<a href='./routes/" + rec + ".html'><p>" + rec + "</p></a>\n\t"
 
-    html_bottom = ""
-    with open("./include/boiler_html_elements/html_bottom.txt", "r") as h_bottom:
-        blines = h_bottom.readlines()
-        for bline in blines:
-            html_bottom += bline
-
     index_output = ""
-    index_output += html_top 
+    index_output += gen_head(title) 
+    index_output += gen_header(title, subtitle, img_src)
     for rec in get_recs():
         index_output += list_recipe(rec)
-    index_output += html_bottom
+    index_output += gen_tail()
     
     with open("./index.html", "w") as html_out:
         html_out.write(index_output)
 
-construct_index()    
+def gen_head(title): 
+    return f'''<html><head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <title>{title}</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./include/main.css"/>
+    </head>'''
+
+def gen_header(title, subtitle, img_src):
+    return f'''
+    <body>
+    <div class="content">
+        <h1>{title}</h1>
+    <div class='teaser-box'>
+    <img class='teaser-img' src='{img_src}'></img>
+    </div>
+    </div>
+    <div class="content" id="content">
+    <h1>{subtitle}</h1>
+    '''
+
+def gen_tail():
+    return f'''
+    </div>
+    </body>
+    </html>
+    '''
+construct_index("Recipebook", "All Recipes", "./include/frying_pan.png")    
