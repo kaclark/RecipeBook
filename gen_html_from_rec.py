ab97ca38bd77d397ae28671763988d7672ec9e90
Keanu Clark <da1andonlyone13@gmail.com>
2023-06-23 18:37:55 -0400

    index.html generated from python based on rec dir. rec pages forthcoming

--- gen_html_from_rec.py
+++ gen_html_from_rec.py
@@ -1,45 +1,39 @@
 #build html files from strings
 #quasi component design strategy
+from pathlib import Path
 
-def construct_index(rec_pages):
-    html_top = """
-    <html>
+def get_recs(file_extension=False):
+    recs = []
+    for x_path in Path("./rec/").glob("*.rec"):
+        if file_extension:
+            recs.append(str(x_path).split("/")[1])
+        else:
+            recs.append(str(x_path).split("/")[1].split(".rec")[0])
+    return recs
 
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-        <title>Recipebook</title>
+def construct_index():
+    html_top = ""
+    with open("./include/boiler_html_elements/html_top.txt", "r") as h_top:
+        lines = h_top.readlines()
+        for line in lines:
+            html_top += line
 
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="./include/main.css"/>
-
-    </head>
-
-    <body>
-        <div class="content">
-            <h1>Recipebook</h1>
-        <div class='teaser-box'>
-        <img class='teaser-img' src='./include/fyring_pan.png'></img>
-        </div>
-        <h2>All Recipies<h2>
-        </div>
-        <div class="content" id="content">
-    """
-    #TODO:print rec as one would in terminal for cli version
     def list_recipe(rec):
-    return "<a href='./routes/" + rec + ".html'><p>" + rec + "</p></a>"
+        return "<a href='./routes/" + rec + ".html'><p>" + rec + "</p></a>\n\t"
 
-    html_bottom = """
-       </div>
+    html_bottom = ""
+    with open("./include/boiler_html_elements/html_bottom.txt", "r") as h_bottom:
+        blines = h_bottom.readlines()
+        for bline in blines:
+            html_bottom += bline
 
-    </body>
-
-    </html>
-    """
     index_output = ""
     index_output += html_top 
-    for rec in rec_pages:
-        index_output += list_recipies(rec)
+    for rec in get_recs():
+        index_output += list_recipe(rec)
     index_output += html_bottom
     
-print(html_top)
-print(html_bottom)
+    with open("./index.html", "w") as html_out:
+        html_out.write(index_output)
+
+construct_index()    
